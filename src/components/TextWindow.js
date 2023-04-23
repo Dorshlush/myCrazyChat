c259e48965da4f47c7b1132edc4b51928f53ed3e
Dorshlush <dor.shlush@gmail.com>
2023-04-23 21:53:51 +0300

    adding functions

--- src/components/TextWindow.js
+++ src/components/TextWindow.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState ,useEffect} from 'react';
 import axios from 'axios';
 
 function TextWindow(props) {
   const [inputText, setInputText] = useState('');
   const [summaryTxt, setSummaryTxt] = useState('');
+  const [generatedTxt,setGeneratedTxt]=useState('')
+  const [continuationText,setContinuationText]=useState('')
 
   const summarizeTxt = async () => {
     const data = { body: inputText };
@@ -16,6 +18,27 @@ function TextWindow(props) {
       console.log(error.message);
     }
   };
+  const generateTxt = async () => {
+    
+    try {
+      const response = await axios.get(
+        ' https://w5xddkfv70.execute-api.us-east-1.amazonaws.com/Prod/generate');
+        setGeneratedTxt(response.data.message);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  const generateTxtContinue = async () => {
+    
+    try {
+      const data={body:inputText}
+      const response = await axios.post(
+        ' https://w5xddkfv70.execute-api.us-east-1.amazonaws.com/Prod/generateTextContinuation',data);
+        setContinuationText(response.data.message);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   const handleChange = (event) => {
     setInputText(event.target.value);
@@ -23,15 +46,23 @@ function TextWindow(props) {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    await generateTxtContinue()
     await summarizeTxt();
   };
-
+  const handleClick = async (event) => {
+    await generateTxt();
+  };
+  
+  useEffect(() => {
+    setInputText(generatedTxt);
+  }, [generatedTxt]);
   return (
     <>
       <div className="text-window">
         <h2>My Crazy Text</h2>
+        <button onClick={()=>handleClick()} className='generateBtn'>Generate Random</button>
         <form onSubmit={handleSubmit}>
-          <textarea value={inputText} onChange={handleChange} />
+          <textarea value={inputText+" "+continuationText} onChange={handleChange} />
           <button type="submit">Compute</button>
         </form>
       </div>
