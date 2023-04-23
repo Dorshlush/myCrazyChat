ceab38ee3f174e1852cb0a9807fb5205dc1d2c25
Dorshlush <dor.shlush@gmail.com>
2023-04-23 19:51:51 +0300

    code cleaning

--- src/components/TextWindow.js
+++ src/components/TextWindow.js
@@ -1,35 +1,49 @@
-import React, { useState, useContext,useEffect } from 'react';
-import { AppContext } from '../context';
-
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function TextWindow(props) {
-  const { setInputText, inputText, summerizeTxt} = useContext(AppContext);
-  const [text, setText] = useState("");
+  const [inputText, setInputText] = useState('');
+  const [summaryTxt, setSummaryTxt] = useState('');
+
+  const summarizeTxt = async () => {
+    const data = { body: inputText };
+    try {
+      const response = await axios.post(
+        'https://w5xddkfv70.execute-api.us-east-1.amazonaws.com/Prod/summarize',
+        data);
+      setSummaryTxt(response.data.message);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   const handleChange = (event) => {
-    setText(event.target.value);
+    setInputText(event.target.value);
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    setInputText(text);
-    summerizeTxt()
-    
+    await summarizeTxt();
   };
-  useEffect(() => {
-    alert(inputText);
-  }, [inputText]);
 
   return (
-    <div className="text-window">
-      <h2>My Crazy Text</h2>
-      <form onSubmit={handleSubmit}>
-        <textarea value={text} onChange={handleChange} />
-        <button type="submit">Compute</button>
-      </form>
-    </div>
+    <>
+      <div className="text-window">
+        <h2>My Crazy Text</h2>
+        <form onSubmit={handleSubmit}>
+          <textarea value={inputText} onChange={handleChange} />
+          <button type="submit">Compute</button>
+        </form>
+      </div>
+      <div className="text-window">
+        <h2>My Crazy Summarizer</h2>
+        <form>
+          <textarea value={summaryTxt} readOnly />
+          <button onClick={() => setSummaryTxt('')}>Reset</button>
+        </form>
+      </div>
+    </>
   );
 }
 
 export default TextWindow;
-
